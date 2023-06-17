062566813ad61404d55716d3976f56e22217719f
burundukoff <burundukoff@re.re>
2023-06-17 19:46:27 +0400

    add strukture

--- stepik_good_good_python/Untitled-1.py
+++ stepik_good_good_python/Untitled-1.py
@@ -1,11 +1,5 @@
-def square_or_square_root(arr):
-    
-    for i,j in enumerate(arr):
-        if j**0.5 == int(j**0.5):
-            arr[i]=int(j**0.5)
-        else:
-            arr[i]=j**2
-    #print(arr)            
-    return arr                
-
-square_or_square_root([4,3,9,7,2,1])
+s = "   fly me   to   the moon  "
+lst=s.split()
+print(lst,s)
+a = len(lst[-1])
+print(a)
